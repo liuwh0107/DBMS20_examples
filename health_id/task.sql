aa336a955e28b3ae28e2c2ef41347e706c380594
karljackab <karljackab@gmail.com>
2020-03-20 13:52:44 +0800

    last half of new health_id task

--- health_id/task.sql
+++ health_id/task.sql
@@ -1,7 +1,8 @@
 -- 1. Find PID of users who traveled UK and were back Taiwan on 2020/03/16
 --- Solution 1
 select inbound.PID
-from (
+from
+(
     select * from airport where country = 'UK'
 ) as airport,
 (
@@ -48,6 +49,46 @@ where user.PID = airport.PID;
 
 -- 3. Find the ID of users who were back from UK or Japan
 --- Solution 1
+select user.ID
+from
+(
+    select TempAirport.*, inbound.PID, inbound.flight_no, inbound.date  from
+    (
+        select * from airport where country = 'UK' or country = 'Japan'
+    ) as TempAirport, inbound
+    where TempAirport.IATA_code = inbound.IATA_code
+) as TempAirport, user
+where TempAirport.PID = user.PID;
 
+--- Solution 2
+select user.ID
+from
+(
+    select airport.*, inbound.PID, inbound.flight_no, inbound.date  from
+    (
+        select * from airport where country = 'UK'
+    ) as airport, inbound
+    where airport.IATA_code = inbound.IATA_code
+) as airport, user
+where airport.PID = user.PID
+union
+select user.ID
+from
+(
+    select airport.*, inbound.PID, inbound.flight_no, inbound.date  from
+    (
+        select * from airport where country = 'Japan'
+    ) as airport, inbound
+    where airport.IATA_code = inbound.IATA_code
+) as airport, user
+where airport.PID = user.PID;
 
--- 4. Find the PID of users who were in the same plant of one confirmed case with his PID as TW0001 on 2020/02/28
+-- 4. Find the PID of users who were in the same plant of one confirmed case with his PID as NQ13219857 on 2020/03/06
+select inbound.PID, inbound.flight_no
+from inbound
+where inbound.flight_no in (
+    select flight_no
+    from inbound
+    where PID = 'NQ13219857'
+    and date = '2020-3-6'
+);
